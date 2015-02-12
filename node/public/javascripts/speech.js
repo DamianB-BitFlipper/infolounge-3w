4dc64add3d517619fe1e7fc518384c2453e0bc8d
rliu42 <rliu42@mit.edu>
2015-02-12 12:55:55 -0500

    support for baymax speech

--- node/public/javascripts/speech.js
+++ node/public/javascripts/speech.js
@@ -2,8 +2,16 @@ var speak = function(phrase) {
 	if(phrase.length > 1 && phrase.length < 200) {
 		$.ajax({url: "http://localhost:8001/demand/" + phrase,
 			success: function(data){return;}
-                });
+            });
 	}
+}
+
+var baymaxSpeak = function(phrase) {
+	phrase = phrase.replace(' ', '+');
+	speech_url = 'http://tts-api.com/tts.mp3?q='+phrase;
+	audio_elem = '<audio src="' + speech_url + '" autoplay></audio>'
+	$('#audio').html(audio_elem);
+	console.log("Baymax speaking...", phrase);
 }
 
 var happyBirthday = function(person) {
@@ -14,7 +22,7 @@ var happyBirthday = function(person) {
 
 var dateToSpeech = function(text) {
 	for (abrev in monthAbrevs) {
-		text = text.replace(monthAbrevs[abrev][0], monthAbrevs[abrev][1]);	
+		text = text.replace(monthAbrevs[abrev][0], monthAbrevs[abrev][1]);
 	}
 	return 	text;
 }
