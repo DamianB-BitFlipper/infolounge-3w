0bcf4ed5e87aec72de28c48a9a2e6ec18ff80b60
rliu42 <rliu42@mit.edu>
2015-02-12 04:15:41 -0500

    dining info

--- node/public/javascripts/menu.js
+++ node/public/javascripts/menu.js
@@ -19,12 +19,13 @@ function getMenu() {
 
         /* Search for images */
         for (var type in data) {
-            var query = data[type];
+            var query = data[type].split('&nbsp;')[0];
+            console.log(query)
             if (menuCache[query]) {
                 document.getElementById("menu").innerHTML += menuCache[query];
             } else {
                 /* Note: DO NOT COPY THIS LINE AND USE IT IN OTHER CODE. IT USES MY PERSONAL GOOGLE API KEY. ~kyc */
-                $.getScript("https://www.googleapis.com/customsearch/v1?key=AIzaSyAngKp74bHK-MZDJZ0E153KVTF0NuzBSrE&cx=013902053734636094783:5ckniww7ndi&q=" + encodeURIComponent(query) + "&callback=menuHandler&searchType=image");
+                $.getScript("https://www.googleapis.com/customsearch/v1?key=AIzaSyBppxBXCGoVew1fU1DjV_Ii8r6kUgred2s&cx=013902053734636094783:5ckniww7ndi&q=" + encodeURIComponent(query) + "&callback=menuHandler&searchType=image");
             }
         }
     });
@@ -32,6 +33,6 @@ function getMenu() {
 
 function menuHandler(response) {
     query = response.queries.request[0].searchTerms;
-    menuCache[query] = "<img src=" + response.items[0].link + " alt='' width=150 style='padding:3px;'>";
+    menuCache[query] = "<img class = 'menu-img' src=" + response.items[0].link + " alt='' width=150 style='padding:3px;'>";
     document.getElementById("menu").innerHTML += menuCache[query];
 }
