c6dc5ae4850002f75baf11c1d21126abbae3c123
rliu42 <rliu42@mit.edu>
2015-02-12 21:59:38 -0500

    style

--- node/public/javascripts/weather.js
+++ node/public/javascripts/weather.js
@@ -5,27 +5,27 @@ function getWeather() {
         unit: 'c',
         success: function(weather) {
             html = '<span class="weather-feel"><div class="row">';
-            html += '<div class="large-3 columns">Current</div>';
-            html += '<div class="large-3 columns">' + weather.forecast[0].day + '</div>';
-            html += '<div class="large-3 columns">' + weather.forecast[1].day + '</div>';
-            html += '<div class="large-3 columns">' + weather.forecast[2].day + '</div>';
+            html += '<div class="small-3 columns">Current</div>';
+            html += '<div class="small-3 columns">' + weather.forecast[0].day + '</div>';
+            html += '<div class="small-3 columns">' + weather.forecast[1].day + '</div>';
+            html += '<div class="small-3 columns">' + weather.forecast[2].day + '</div>';
             html += '</div></span>';
 
 		console.log(weather.forecast[0].day);
-		console.log(weather.temp);            
+		console.log(weather.temp);
 
             html += '<div class="row">';
-            html += '<div class="large-3 columns"><img src="images/weather/' + weather.code + '.png"><br/>';
+            html += '<div class="small-3 columns"><img src="images/weather/' + weather.code + '.png"><br/>';
             html += '<span class="weather-temp">' + (weather.alt.temp) + '&deg; (' + weather.temp + '&deg;C)</span>';
-            
+
 	    if (weather.temp != weather.wind.chill) {
 	    html += '<br/><span class="weather-feel">Feel ' + ((weather.wind.chill)*(9/5)+32).toFixed() + '&deg; (' + weather.wind.chill + '&deg;C)</span>';}
-            
-	    html += '</div><div class="large-3 columns"><img src="images/weather/' + weather.forecast[0].code + '.png"><br/>';
+
+	    html += '</div><div class="small-3 columns"><img src="images/weather/' + weather.forecast[0].code + '.png"><br/>';
             html += '<span class="weather-temp">' + weather.forecast[0].alt.high + '&deg; / ' + weather.forecast[0].alt.low + '&deg;</span></div>';
-            html += '<div class="large-3 columns"><img src="images/weather/' + weather.forecast[1].code + '.png"><br/>';
+            html += '<div class="small-3 columns"><img src="images/weather/' + weather.forecast[1].code + '.png"><br/>';
             html += '<span class="weather-temp">' + weather.forecast[1].alt.high + '&deg; / ' + weather.forecast[1].alt.low + '&deg;</span></div>';
-            html += '<div class="large-3 columns"><img src="images/weather/' + weather.forecast[2].code + '.png"><br/>';
+            html += '<div class="small-3 columns"><img src="images/weather/' + weather.forecast[2].code + '.png"><br/>';
             html += '<span class="weather-temp">' + weather.forecast[2].alt.high + '&deg; / ' + weather.forecast[2].alt.low + '&deg;</span></div>';
             html += '</div>';
 
@@ -36,4 +36,3 @@ function getWeather() {
         }
     });
 };
-
