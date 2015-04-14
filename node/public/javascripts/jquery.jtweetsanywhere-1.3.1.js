15be4dd014101166a871f256e4bfe11e8dd0f3f4
rliu42 <rliu42@mit.edu>
2015-04-13 20:45:55 -0400

    meta shenanigans

--- node/public/javascripts/jquery.jtweetsanywhere-1.3.1.js
+++ node/public/javascripts/jquery.jtweetsanywhere-1.3.1.js
@@ -2615,3 +2615,38 @@ JTA_I18N.addResourceBundle('jTweetsAnywhere', 'en',
 		$.getJSON(url, callback);
 	};
 })(jQuery);
+
+  var people = [
+    ['tiffwang', 'January 1, 1995 00:00:00', 'Tiffany', '#00FFFF'],
+    ['xtnbui', 'January 4, 1996 00:00:00', 'Xuan', 'green'],
+    ['jnation', 'February 4, 1989 00:00:00', 'Josh RT', 'red'],
+    ['yzhang17', 'February 13, 1996 00:00:00', 'Yaning', '#29F0E2'],
+    ['evayeung', 'February 13, 1993 00:00:00', 'Eva', "#ADFFAD"],
+    ['akwasio', 'February 15, 1995 00:00:00', 'Akwasi', 'green'],
+    ['oropp', 'March 4, 1996 00:00:00', 'Or', '#7D26CD'],
+    ['jwei314', 'March 14, 1993 04:00:00', 'Jenny W'],
+    ['jenniez', 'March 24, 1993 00:00:00', 'Jennie Z'],
+    ['lotusez3', 'March 24, 1995 00:00:00', 'Elton', '#D391DB'],
+    ['mwu2015', 'April 8, 1993 00:00:00', 'Michael Wu', 'red'],
+    ['juesato', 'April 21, 1995 00:00:00', 'Jonathan U'],
+    ['cmzhang', 'October 8, 1993 00:00:00', 'Clare', '#000047'],
+    ['eurahko', 'May 17, 1993 00:00:00', 'Eurah', '#226A87'],
+    ['sallylin', 'May 22, 1993 00:00:00', 'Sally'],
+    ['tricias', 'May 23, 1995 00:00:00', 'Tricia', '#00ccff'],
+    ['huangjd', 'May 26, 1994 00:00:00', 'William', '#990033'],
+    ['smmchugh', 'May 26, 1995 00:00:00', 'Steph', 'green'],
+    ['abrashen', 'June 2, 1994 00:00:00', 'Abra'],
+    ['ncolant', 'July 21, 1995 00:00:00', 'Noelle', '#00CED1'],
+    ['stalyc', 'August 10, 1993 00:00:00', 'Staly', 'green'],
+    ['anitaliu', 'August 11, 1998 00:00:00', 'Anita', '#551a8b'],
+    ['chenbon', 'August 28, 1996 00:00:00', 'Bonnie'],
+    ['kkarthur', 'August 29, 1995 00:00:00', 'Bena', 'green'],
+    ['mabrams', 'September 2, 1995 00:00:00', 'Melanie', "#9F7EE6"],
+    ['ksaleeby', 'September 20, 1994 00:00:00', 'Kyle'],
+    ['harlin', 'September 23, 1993 00:00:00', 'Harlin'],
+    ['julia_h', 'July 24, 1996 00:00:00', 'Julia'],
+    ['joshbs', 'September 26, 1996 00:00:00', 'Josh Josh'],
+    ['rliu42', 'October 10, 1994 00:00:00', 'Runpeng', 'black'],
+    ['zsheinko', 'October 28, 1994 00:00:00', 'Zoe', 'red'],
+    ['dfavela', 'November 1, 1996 00:00:00', 'Favela', 'red']
+      ];
