729caea848ecdd0cba895d670d1961196e5d5f64
rliu42 <rliu42@mit.edu>
2015-05-11 20:34:06 -0400

    priori incantatum

--- node/public/javascripts/news.js
+++ node/public/javascripts/news.js
@@ -1,13 +1,17 @@
 var newsURL = '/news.json';
+var once = false;
 
 function getNews() {
-    $.getJSON(newsURL, function(data) {
-        if (jQuery.isEmptyObject(data)) {
-            $("#newspanel").slideUp("slow");
-            return;
-        }
-        console.log(data);
-        $("#newspanel").slideDown("slow");
-        $("#news").html(data.notification);
-    });
+	if (! (graduation && once) ) {
+	    $.getJSON(newsURL, function(data) {
+	    	once = true;
+	        if (jQuery.isEmptyObject(data)) {
+	            $("#newspanel").slideUp("slow");
+	            return;
+	        }
+	        //console.log(data);
+	        $("#newspanel").slideDown("slow");
+	        $("#news").html(data.notification);
+	    });
+	}
 };
