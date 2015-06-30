0594892384b5d9c93c57a0a4ac8e1235add94299
rliu42 <rliu42@mit.edu>
2015-06-30 08:22:08 +0800

    MIT emergencies update

--- node/public/javascripts/institute_alerts.js
+++ node/public/javascripts/institute_alerts.js
@@ -15,14 +15,16 @@ $.getJSON('/institute_alerts.json', function(data){
 	  if (data[alert].length > 20) {
 		if (lastAlert !== data[alert]) {
 		    lastAlert = data[alert];
-		    speak("This is Beymax with an MIT emergency update: " + data[alert].split('. ')[0]);
+		    speak("This is Safety Baymax with an M.I.T. emergency update: " + data[alert].replace("Updated ", "").split('. ')[0].replace(/MIT/, "M.I.T."), "", "", "");
 		}
-         	elem += ("<p style='font-size: 32px'><b>" + alert + "</b></p>" + "<br>" + data[alert])
+         	elem += ("<p style='font-size: 32px'><b>" + alert + "</b></p>" + "<br>" + data[alert].replace("Updated ", ""))
     }
 	}
 	if (elem.length > 0){
-	 $("#MITalerts").html(elem);
      $('#MITalertspanel').slideDown("slow");
+     $('.EMRGpanel').slideDown("slow", function() {
+     	$("#MITalerts").html(elem).slideDown();
+     });
 	} else {
 	   $('#MITalertspanel').slideUp('slow');
 	}
